ae2b837d4e97c8b21ae0319e8a02425f8290333e
yossiyossi <yossi.hertzberg@gmail.com>
2019-03-08 13:23:22 -0800

    basic make_date_list working

--- lecture-gen.py
+++ lecture-gen.py
@@ -4,6 +4,8 @@
 import argparse
 import pytest
 import datetime
+import dateutil
+from dateutil.parser import *
 
 legal_days_of_week="MTWRF"
 
@@ -15,7 +17,8 @@ def validate_days_of_week(days_of_week):
 
 
 def validate_date(date):
-     result = datetime.utcfromtimestamp(date) # raises ValueError if illegal value
+     result = datetime.datetime.utcfromtimestamp(date) # raises ValueError if illegal value
+     #I am getting erros from the above call, using parse, we might not need this function
      return result
 
 def validate_start_date(date):
@@ -43,7 +46,7 @@ def n_days_ahead(start_datetime, days):
     """
     Return a datetime that is start_datetime plus days into the future
     """
-    delta = datetime.timedelta(days)
+    delta = datetime.timedelta(days = days)
     return start_datetime + delta
 
 def days_for_this_week(start_datetime):
@@ -64,10 +67,15 @@ def days_without_holidays(days_of_week, holiday_list):
     Given a tuple for the two lecture days of the week, returns a list
     of valid dates, removing any that clash with holidays
     """
+    sanitized_holidays = []
+    for h in holiday_list:
+        sanitized_holidays.append(parse(h))
+
+
     sanitized_dates = []
-    if days_of_week[0] not in holiday_list:
+    if days_of_week[0] not in sanitized_holidays:
         sanitized_dates.append(days_of_week[0])
-    if days_of_week[1] not in holiday_list:
+    if days_of_week[1] not in sanitized_holidays:
         sanitized_dates.append(days_of_week[1])
 
     return sanitized_dates
@@ -77,33 +85,31 @@ def make_date_list(start_date, weeks, days_of_week, holiday_list):
     return list of date strings in yyyy-mm-dd format
 
     Example:
+
+    TODO shouldn't the 3 below be 2? As this is for 2 weeks?
      make_date_list("2019-03-31",3,"MW",["2019-04-08","2019-04-09"]) =>
        ["2019-04-01","2019-04-03","2019-04-10"]
     """
 
     validate_days_of_week(days_of_week)
-    start_datetime = validate_date(start_date)
-    final_datetimes = []
+    #start_datetime = validate_date(parse(start_date))
+    start_datetime = parse(start_date)
 
+    final_datetimes = []
     for i in range(weeks):
         start_of_week_datetime = add_weeks(start_datetime,i)
         days_this_week_unsanitized = days_for_this_week(start_of_week_datetime)
         days_this_week_sanitized = days_without_holidays(days_this_week_unsanitized, holiday_list)
         final_datetimes.append(days_this_week_sanitized)
 
+    return final_datetimes
+
 
 def lecture_gen():
    pass
 
 
-
-
 if __name__=="__main__":
     #lecture_gen()
-    """
-    d1 = datetime.date.today()
-    print(d1)
-    d2 = n_days_ahead(d1, 5)
-    print(d2)
-    """
-    make_date_list("2019-03-31",3,"MW",["2019-04-08","2019-04-09"])
+    valid_dates = make_date_list("2019-04-01",2,"MW",["2019-04-08","2019-04-09"])
+    print(valid_dates)
